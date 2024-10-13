97297760321f5e8d1fd9961630cc1fa371a5bc50
StrongMeders <strongmeders@gmail.com>
2024-10-13 19:25:03 -0400

    transformação do elemento avatar em module + transformação do layout em responsivo

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,8 @@
-import styles from "./Sidebar.module.css"
 import { PencilLine } from "phosphor-react"
+
+import { Avatar } from "./Avatar"
+
+import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
 	return (
@@ -9,10 +12,7 @@ export function Sidebar() {
 				src="https://media.discordapp.net/attachments/1294897491904430090/1294897569604046932/photomode_22092024_040610.png?ex=670caed6&is=670b5d56&hm=0ee6cf87a96ede6dde86e17405d93779a81f201bc432699b83476e4f9d3932f5&=&format=webp&quality=lossless&width=500&height=280"
 			/>
 			<div className={styles.profile}>
-				<img
-					className={styles.avatar}
-					src="https://github.com/strongmeders.png"
-				/>
+				<Avatar src="https://github.com/strongmeders.png" />
 
 				<strong>StrongMeders</strong>
 				<span>Web Developer & UI Designer</span>
